72c2de7cdeccad8df9e9aacc125bdce327d297d9
Subham0112 <subukhatiwada123@gmail.com>
2024-03-25 20:33:31 +0545

    modifying

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 
 let imageShow=document.getElementById('img')
-async function randomImg(){
+ function randomImg(){
     let i=Math.floor(Math.random()*1084)
-    let a=fetch(`https://picsum.photos/id/${i}/700`)
+    let a=fetch(`https://picsum.photos/id/${i}/800`)
     a.then(function(value){
-      return value
-    }).then(function(value2){
-        imageShow.src=value2.url;
+      imageShow.src=value.url;
          console.log("id:"+ i)
-
-          
     }).catch(function(error){
-    console.log("error on loading image ")
+    console.log(error)
     })
     }
 
